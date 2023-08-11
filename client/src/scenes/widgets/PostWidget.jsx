9f2bbaade1a2b210e8a878eb97cd7c8ab4315ae9
Andres Sierra <a.sierra.soft.developer@gmail.com>
2023-08-11 14:43:55 -0500

    se actualiza el diseño y botones

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -6,7 +6,7 @@ import {
   ShareOutlined,
   AddCommentRounded,
 } from "@mui/icons-material";
-import { Box, Divider, IconButton, Typography, useTheme, InputBase, Button, Snackbar } from "@mui/material";
+import { Box, Divider, IconButton, Typography, useTheme, InputBase, Button, Snackbar, Fade } from "@mui/material";
 import MuiAlert from '@mui/material/Alert';
 import FlexBetween from "components/FlexBetween";
 import Friend from "components/Friend";
@@ -44,6 +44,11 @@ const PostWidget = ({
   const { palette } = useTheme();
   const main = palette.neutral.main;
   const primary = palette.primary.main;
+  const medium = palette.neutral.medium;
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
 
   const patchLike = async () => {
     const response = await fetch(`${URL}/posts/${postId}/like`, {
@@ -59,7 +64,6 @@ const PostWidget = ({
   };
 
   const patchComment = async () => {
-
     listComment.push({
       userIdComment:`${user._id}`,
       name:`${user.firstName} ${user.lastName}`,
@@ -88,10 +92,6 @@ const PostWidget = ({
       console.error("Error:", error.message);
     }
   };
-
-  const Alert = React.forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-  });
 
   const showSnackbarSuccess = () => {
     setOpenSnackbarSuccess(true);
@@ -147,90 +147,103 @@ const PostWidget = ({
             </IconButton>
             <Typography>{likeCount}</Typography>
           </FlexBetween>
-
           <FlexBetween gap="0.3rem">
             <IconButton onClick={() => setIsComments(!isComments)}>
               <ChatBubbleOutlineOutlined />
             </IconButton>
             <Typography>{comments.length}</Typography>
           </FlexBetween>
+          <FlexBetween gap="0.3rem">
+            <IconButton>
+              <ShareOutlined />
+            </IconButton>
+          </FlexBetween>
         </FlexBetween>
-
-        <IconButton>
-          <ShareOutlined />
-        </IconButton>
       </FlexBetween>
       {isComments && (
-        
-        <Box mt="0.5rem">
-          <FlexBetween gap="1.5rem">
-            <InputBase
-              placeholder="Comenta lo que sea..."
-              onChange={(e) => setCommentPost(e.target.value)}
-              value={commentPost}
-              sx={{
-                width: "100%",
-                backgroundColor: palette.neutral.light,
-                borderRadius: "2rem",
-                padding: "0.2rem 2rem",
-              }}
-              autoFocus={true}
-            />
-            <Button
-              disabled={!commentPost}
-              onClick={patchComment}
-              sx={{
-                color: palette.background.alt,
-                backgroundColor: palette.primary.main,
-                borderRadius: "3rem",
-              }}
-            >
-              <AddCommentRounded />
-            </Button>
-            <Snackbar
-              open={openSnackbarSuccess}
-              autoHideDuration={3000}
-              onClose={handleCloseSnackbarSuccess}
-            >
-              <Alert onClose={handleCloseSnackbarSuccess} severity="success" color="info" sx={{ width: '100%' }}>
-                Comentario creado!
-              </Alert>
-            </Snackbar>
-            <Snackbar
-              open={openSnackbarError}
-              autoHideDuration={3000}
-              onClose={handleCloseSnackbarError}
-            >
-              <Alert onClose={handleCloseSnackbarError} severity="error" sx={{ width: '100%' }}>
-                Error al crear el comentario!
-              </Alert>
-            </Snackbar>
-          </FlexBetween>
-          <br />
-          {comments.map((comment, i) => (
-            <Box key={`${name}-${i}`}>
-              <Divider />
-              <Typography
-                color={main}
-                variant="h7"
-                fontWeight="500"
-                sx={{
-                  "&:hover": {
-                    color: palette.primary.light,
-                    cursor: "pointer",
-                  },
-                }}
-                onClick={() => navigate(`/profile/${comment.userIdComment}`)}
-              >
-                {comment.name}
-              </Typography>
-              <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
-                {comment.message}
-              </Typography>
-            </Box>
-          )).reverse()}
-          <Divider />
-        </Box>
+        <Fade  in={isComments}>
+          <Box mt="0.5rem">
+            <form onSubmit={(e) => { e.preventDefault(); patchComment(); }}>
+              <FlexBetween gap="1.5rem">
+                  <InputBase
+                    placeholder="Comenta lo que sea..."
+                    onChange={(e) => setCommentPost(e.target.value)}
+                    value={commentPost}
+                    sx={{
+                      width: "100%",
+                      backgroundColor: palette.neutral.light,
+                      borderRadius: "2rem",
+                      padding: "0.2rem 2rem",
+                    }}
+                    autoFocus={true}
+                  />
+                  <Button
+                    disabled={!commentPost}
+                    // onClick={patchComment}
+                    type='submit'
+                    sx={{
+                      color: palette.background.alt,
+                      backgroundColor: palette.primary.main,
+                      borderRadius: "3rem",
+                    }}
+                  >
+                    <AddCommentRounded />
+                  </Button>
+                <Snackbar
+                  open={openSnackbarSuccess}
+                  autoHideDuration={3000}
+                  onClose={handleCloseSnackbarSuccess}
+                >
+                  <Alert onClose={handleCloseSnackbarSuccess} severity="success" color="success" sx={{ width: '100%' }}>
+                    Comentario creado!
+                  </Alert>
+                </Snackbar>
+                <Snackbar
+                  open={openSnackbarError}
+                  autoHideDuration={3000}
+                  onClose={handleCloseSnackbarError}
+                >
+                  <Alert onClose={handleCloseSnackbarError} severity="error" sx={{ width: '100%' }}>
+                    Error al crear el comentario!
+                  </Alert>
+                </Snackbar>
+              </FlexBetween>
+            </form>
+            <br />
+            {comments.length !== 0 ? (
+              <>
+                {comments.map((comment, i) => (
+                  <Box key={`${name}-${i}`}>
+                    <Divider />
+                    <Typography
+                      color={main}
+                      variant="h7"
+                      fontWeight="500"
+                      sx={{
+                        "&:hover": {
+                          color: palette.primary.light,
+                          cursor: "pointer",
+                        },
+                      }}
+                      onClick={() => navigate(`/profile/${comment.userIdComment}`)}
+                    >
+                      {comment.name}
+                    </Typography>
+                    <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
+                      {comment.message}
+                    </Typography>
+                  </Box>
+                )).reverse()}
+              </>
+            ) : (
+              <>
+                <Typography color={medium}>No hay comentarios...</Typography>
+              </>
+            )}
+            
+            <Divider />
+          </Box>
+        </Fade>
       )}
     </WidgetWrapper>
   );
