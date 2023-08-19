e9ab1e99ff967d5e0793d59c709b8910b3f077f9
Andres Sierra <a.sierra.soft.developer@gmail.com>
2023-08-19 15:37:39 -0500

    config to deploy

--- client/src/Url.js
+++ client/src/Url.js
@@ -1,1 +1,3 @@
 export const URL = 'http://localhost:3001'  // Url Current Server
+// export const URL = 'http://localhost:3001'  // Url Dev
+// export const URL = 'https://red-social-mern.vercel.app'  // Url Prod
