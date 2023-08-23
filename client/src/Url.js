6f623634c903225d49ebb510652a56f1cc96db9f
Andres Sierra <a.sierra.soft.developer@gmail.com>
2023-08-23 17:46:52 -0500

    updated

--- client/src/Url.js
+++ client/src/Url.js
@@ -1,3 +1,3 @@
 export const URL = 'http://localhost:3001'  // Url Current Server
 // export const URL = 'http://localhost:3001'  // Url Dev
-// export const URL = 'https://red-social-mern.vercel.app'  // Url Prod
+// export const URL = ''  // Url Prod
